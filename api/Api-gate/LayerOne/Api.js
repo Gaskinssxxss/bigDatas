9f68ea54b6221be4b7eb11fd196ba8b1c571b2b7
Trotsky <119833733+Gaskinssxxss@users.noreply.github.com>
2025-09-21 17:58:42 +0800

    Update Api.js

--- api/Api-gate/LayerOne/Api.js
+++ api/Api-gate/LayerOne/Api.js
@@ -8,7 +8,7 @@ app.use(morgan("dev"));
 const collectionName = "raw_data";
 
 const mongoURL =
-  "mongodb://bmblx999:indra999@ac-aitzldg-shard-00-00.uoyd2xl.mongodb.net:27017,ac-aitzldg-shard-00-01.uoyd2xl.mongodb.net:27017,ac-aitzldg-shard-00-02.uoyd2xl.mongodb.net:27017/rawData?replicaSet=atlas-vrw4js-shard-0&ssl=true&authSource=admin";
+  "";
 
 mongoose
   .connect(mongoURL, {
